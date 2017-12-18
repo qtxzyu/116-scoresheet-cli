188bf8dc42c40d9ea5249f8c9d4bc11716c911e7
qtxzyu <574145771@qq.com>
2017-12-18 11:33:07 +0800

    remain one little chinese problem of messy code

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -39,18 +39,49 @@ describe('getChildMenuString()', () => {
 describe("addStudentAchievement()", () => {
 	it('input the right format of studentinfo', () => {
 		let num = '1';
-		let inputString = '姓名,学号,民族,班级,学科:成绩,学科:成绩,学科:成绩,学科:成绩';
+		let inputString = '马零,0,汉,1,数学:75,语文:95,英语:80,编程:80';
 		let name = inputString.split(',')[0];
 		spyOn(console, 'log');
 		main(num, inputString);
 		expect(console.log).toHaveBeenCalledWith('学生'+ name + '的成绩被添加');
 	});
-	it('input the right format of studentinfo', () => {
+	it('input the wrong format of studentinfo', () => {
 		let num = '1';
-		let inputString = '姓名,学号,民族,学科:成绩,学科:成绩,学科:成绩,学科:成绩';
+		let inputString = '张三,1,汉,数学:75,语文:95,英语:80,编程:80';
 		let name = inputString.split(',')[0];
 		spyOn(console, 'log');
 		main(num, inputString);
 		expect(console.log).toHaveBeenCalledWith('请输入学生信息（格式：姓名, 学号, 民族, 班级, 学科: 成绩, ...');
+	});
+});
+
+describe("buildReport()", () => {
+	it('input the right format to get the report', () => {
+		let num = '2';
+		let inputString = '1,2';
+		let name = inputString.split(',')[0];
+		spyOn(console, 'log');
+		main(num, inputString);
+		// expect(console.log).toHaveBeenCalledWith('成绩单\n' + 
+    // '姓名|数学|语文|英语|编程|平均分|总分 \n' + '========================\n' + '张三|75|95|80|80|82.5|330\n' + 
+    // '李四|85|80|70|90|81.25|325\n' + '========================\n' + '全班总分平均数：327.5\n' + 
+     // '全班总分中位数：327.5');
+	 expect(console.log).toHaveBeenCalledWith('成绩单\n'+
+    '姓名|数学|语文|英语|编程|平均分|总分\n'+
+    '========================\n'+
+    '张三|75|95|80|80|82.5|330\n'+
+    '李四|85|80|70|90|81.25|325\n'+
+    '========================\n'+
+    '全班总分平均数：327.5\n'+
+    '全班总分中位数：327.5');
+	 
+	});
+	it('input the wrong format', () => {
+		let num = '2';
+		let inputString = 'hhh:hhh';
+		let name = inputString.split(',')[0];
+		spyOn(console, 'log');
+		main(num, inputString);
+		expect(console.log).toHaveBeenCalledWith(    '请按正确的格式输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：');
 	});
 });
